e9f868cdd84239bc85442bc2c79b90558a30355b
Shibi S. Kumar <dev.shibi1306@gmail.com>
2021-02-12 12:09:52 +0530

    updated Problem 3

--- prime_factors.hs
+++ prime_factors.hs
@@ -2,7 +2,7 @@ maxPrimeFactors n = maximum (getPrimeFactors n 2)
 
 getPrimeFactors :: (Integral a) => a -> a -> [a]
 getPrimeFactors n i
-		| i > (n `div` 2) = [n]
+		| i > (floor . sqrt . fromIntegral) n = [n]
 		| n `mod` i == 0 = [i] ++ getPrimeFactors (n `div` i) i
 		| i == 2 = getPrimeFactors n (i + 1)
 		| otherwise = getPrimeFactors n (i + 2)
